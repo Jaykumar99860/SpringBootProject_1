12602e2304c39ae79aa9191e49e347207bf5caac
Jay <jaykumar99860@gmail.com>
2023-11-22 02:20:30 +0530

    Added the PUT method

--- src/main/java/com/example/ProjectJK_1123_2/UserController.java
+++ src/main/java/com/example/ProjectJK_1123_2/UserController.java
@@ -5,11 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+
 public class UserController {
 @Autowired
 UserService userservice;
+
+@Autowired
+UserRepository userRepository;
 	
 	
 	
@@ -18,8 +24,15 @@ public String welcome() {
 	return "Hi, Welcome to my Project";
 }
 
-@RequestMapping(value="/getUsers",method=RequestMethod.GET)
+@RequestMapping(value="/getUsers",method=RequestMethod.GET, produces = "application/json")
 public String getUsers() {
 	return userservice.getAllUsers();
 }
+
+@RequestMapping(value="/addUsers", method=RequestMethod.PUT)
+public String addUser(@RequestBody  User user) {
+	userRepository.save(user);
+	return "User Data saved";
+}
+
 }
--- src/main/java/com/example/ProjectJK_1123_2/UserService.java
+++ src/main/java/com/example/ProjectJK_1123_2/UserService.java
@@ -4,19 +4,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.ProjectJK_1123_2.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class UserService {
 	@Autowired
 	UserRepository userRepository;
 	
-	
-	
-	public String getAllUsers() {
+	public String getAllUsers () {
 		userRepository.save(new User(1,"Jay","Bangalore"));
 		userRepository.save( new User(2, "Amit", "Bangalore"));
 		List<User> lsuser = userRepository.findAll();
-		return String.valueOf(lsuser.get(0));
+		String result = "";
+		try {
+		ObjectMapper mapper = new ObjectMapper();
+		for(User user : lsuser) {
+			result+= mapper.writeValueAsString(user)+"\n";
+		}
+		}catch(Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		return result;
 	}
 }
